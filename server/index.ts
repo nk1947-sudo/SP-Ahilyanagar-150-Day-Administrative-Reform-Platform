7b50717748a1ed5aa111436cbab900d02c07a141
Dawell-Tech <tonimbalkarkaustubh1947@gmail.com>
2025-06-09 11:10:49 +0530

    Initial commit: SP Ahilyanagar 150-Day Administrative Reform Platform

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -62,8 +63,8 @@ app.use((req, res, next) => {
   const port = 5000;
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "127.0.0.1", // Change from "0.0.0.0" to "127.0.0.1"
+    // Remove the reusePort line completely
   }, () => {
     log(`serving on port ${port}`);
   });
